32b913dfda3bbb338b7a01b0b2d62b287e61ade2
braden <bradenjbagby@gmail.com>
2021-05-26 13:12:05 -0500

    prepare

--- server/constants.js
+++ server/constants.js
@@ -2,7 +2,7 @@ const PORT = 7788;
 
 module.exports = Object.freeze({
     PORT: PORT,
-    REDIRECT_URI: `http://localhost:${PORT}/authResult`,
+    REDIRECT_URI: `http://ec2-3-142-189-223.us-east-2.compute.amazonaws.com/authResult`,
     SPOTIFY_STATE_KEY: 'spotify_auth_state',
     SPOTIFY_TOKEN_KEY: 'spotify_auth_token',
     SPOTIFY_REFRESH_TOKEN_KEY: 'spotify_auth_refresh_token',
