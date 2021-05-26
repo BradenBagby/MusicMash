56eb748483a5ea1b132de9fbd998aa154628a710
braden <bradenjbagby@gmail.com>
2021-05-26 14:58:40 -0500

    built

--- app/flutter-web-app/music_mash/build/web/flutter_service_worker.js
+++ app/flutter-web-app/music_mash/build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "ff748468196872da9347249e8d054c58",
-"index.html": "7df81587b439b0390cafaff9e84cfa68",
-"/": "7df81587b439b0390cafaff9e84cfa68",
-"main.dart.js": "9f071a7033714b2c694b11d16304095f",
+"index.html": "f865f621f4abf7bc99f163a79ae6c578",
+"/": "f865f621f4abf7bc99f163a79ae6c578",
+"main.dart.js": "fa4edfb777186ecec6a3b4036cd9ff9d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
